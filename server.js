de0b407bb3427da6016c95825fb65a51df2a5f27
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-04 15:42:27 +0530

    mongoose connection to URL added

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("connection to database successfull"));
 
 app.listen(port, () => {
   console.log(`Server live on : ${port}`);
