53a8e8b791b3b7e72d4a023d693dbd4a1abb9342
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-04 16:40:20 +0530

    get request added

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+const bycrypt = require("bcrypt");
+const routes = require("./routes/user");
 const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
@@ -10,6 +13,8 @@ const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("connection to database successfull"));
 
+app.use(bodyParser.json());
+app.use("/", routes);
 app.listen(port, () => {
   console.log(`Server live on : ${port}`);
 });
