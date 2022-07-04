73714712b912e36e941c6cb497ac400727144756
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-04 17:03:29 +0530

    post request added

--- routes/user.js
+++ routes/user.js
@@ -11,4 +11,21 @@ router.get("/users", async (req, res) => {
   }
 });
 
+router.post("/users", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const userPTR = new User({
+    username,
+    password,
+  });
+
+  try {
+    const newUser = await userPTR.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
