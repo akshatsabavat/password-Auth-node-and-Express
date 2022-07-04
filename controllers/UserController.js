6d5325bbc689ef85b323ee673c29d05176d3010c
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-04 17:40:47 +0530

    hashed password data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,15 +11,13 @@ exports.getUsers = async (req, res) => {
 };
 
 exports.addUser = async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  const userPTR = new User({
-    username,
-    password,
-  });
-
   try {
+    const salt = await bycrypt.genSalt(10); // higher the salt value better the hash, more secure but the longer it takes to make the hash
+    const hashedPassword = await bycrypt.hash(req.body.password, salt);
+    const userPTR = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
     const newUser = await userPTR.save();
     res.status(201).send(newUser);
   } catch (err) {
